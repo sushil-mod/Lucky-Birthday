4de5f53ab427bad445f903d5680f5d93d1ac720f
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2021-09-29 23:45:14 +0530

    feat: added  funtion

--- app.js
+++ app.js
@@ -3,5 +3,17 @@ var luckyNum = document.querySelector("#lucky-number");
 var checkNumber = document.querySelector("#check-number");
 
 checkNumber.addEventListener('click',function getDateNum() {
-    console.log(dateOfBirth.value,luckyNum.value);
+    var birthDate = dateOfBirth.value;
+    var sum = calSum(birthDate);
+    console.log(sum);
 });
+
+function calSum(birthDate){
+    birthDate = birthDate.replaceAll("-","");
+    let sum=0;
+    for (let i=0;i<birthDate.length;i++)
+    {
+        sum+=Number(birthDate.charAt(i));
+    }
+    return sum;
+}
